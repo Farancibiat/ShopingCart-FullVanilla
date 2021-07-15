1718adacce76f47a8d5fbe9d2385e7cda478affc
Pehuen <arancibiat.felipe@gmail.com>
2021-07-14 20:07:38 -0400

    responsive navbar done

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-app.js
+
+function loadApi(){
+    fetch("http://www.omdbapi.com/?apikey=4fa12616&i=tt3896198")
+    .then(response=>response.json())
+    .then(search=>{
+        console.log(search.Title)
+    })
+    .catch(e=>{
+        console.log("error :", e)
+    })
+}
+
+function respBehavior() {
+    var x = document.getElementById("myMenu");
+    if (x.className === "menu") {
+      x.className += " responsive";
+    } else {
+      x.className = "menu";
+    }
+  }
